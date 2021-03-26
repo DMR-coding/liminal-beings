d8ad638630da6b327b7b369dea3926298faf1b48
Daniel Maxwell-Ross <daniel@maxwell-ross.us>
2021-03-25 23:12:01 -0700

    automate adding headers and footers

---
+++ add_tags.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+
+
+
+BEGINNING = '''
+<!-- This section is required only once per page, before any videos appear. -->
+<link type="text/css" rel="stylesheet" href="https://ozplayer.global.ssl.fastly.net/3.5/ozplayer-core/ozplayer.min.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://ozplayer.global.ssl.fastly.net/3.5/ozplayer-skin/highlights-blue.css" media="all" />
+<link type="text/css" rel="stylesheet" href="https://ozplayer.global.ssl.fastly.net/3.5/transcript.css" media="all" />
+<script type="text/javascript" src="https://ozplayer.global.ssl.fastly.net/3.5/ozplayer-core/mediaelement.min.js"></script>
+'''
+
+
+END = '''
+<!-- This section is once per page, but after all the videos -->
+<script type="text/javascript" src="https://ozplayer.global.ssl.fastly.net/3.5/ozplayer-core/ozplayer.free.js"></script>
+<script type="text/javascript" src="https://ozplayer.global.ssl.fastly.net/3.5/ozplayer-lang/en.js"></script>
+'''
+
+doc = None
+with open("./index.html", "r") as fp:
+    soup = BeautifulSoup(fp, features="html.parser")
+
+    soup.head.append(BEGINNING)
+    soup.body.append(END)
+
+    doc = str(soup)
+
+with open("./index.html", "w") as fp:
+    fp.write(doc)
